befc26cc53263a87a7ee9875150c2a3720c5faf8
Judas-u <adishwarraj9905@gmail.com>
2025-06-23 00:18:17 +0530

    Lets-Begin

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express();  // ← You need this line!
+const app = express();  
 const mongoose = require("mongoose");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
 
 app.get("/", (req, res) => {
     res.send("Hi I am root !");
--- info.md
+++ info.md
@@ -1,8 +1,9 @@
 <!-- Packages -->
 1. npm init -y
-2. npm i ejs
-3. npm i mongoose
-4. touch app.js
+2. npm i express
+3. npm i ejs
+4. npm i mongoose
+5. touch app.js
 
 <!-- Code  -->
 1. app.js (code-line : 1 to 11)
