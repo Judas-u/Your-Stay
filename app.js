5a1aab2e73d30240eb25d844c1baf0c1a9ffaea4
Judas-u <adishwarraj9905@gmail.com>
2025-06-23 16:12:09 +0530

    Listing-Model

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();  
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log(err);
+})
+
+
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
@@ -11,6 +21,21 @@ async function main() {
 app.get("/", (req, res) => {
     res.send("Hi I am root !");
 });
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "Beachside Stay",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("successful testing");
+})
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
