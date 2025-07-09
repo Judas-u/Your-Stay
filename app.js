9e2ca03218ecaec1438aa971cd9a7aed58afa698
Judas-u <adishwarraj9905@gmail.com>
2025-07-09 21:47:52 +0530

    Error-Handling

--- app.js
+++ app.js
@@ -97,13 +97,13 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 // 404 Handler
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found", 404));
+  next(new ExpressError("Page not found!", 404));
 });
 
 // Error Handler
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error", { err });
+  res.status(statusCode).render("error", { message });
 });
 
 // Start server
