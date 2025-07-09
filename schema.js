1eaef2c766ac592411d3cb6202651570a74b39be
Judas-u <adishwarraj9905@gmail.com>
2025-07-09 22:58:28 +0530

    Fixed-all-Errors

--- schema.js
+++ schema.js
@@ -1,12 +1,15 @@
 const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title : Joi.string().required(),
-        description : Joi.string().required(),
-        image : Joi.string().allow("", null),
-        price : Joi.number().required(),
-        location : Joi.string().required().min(0),
-        country : Joi.string().required(),
-    }).required
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().required(),
+        location: Joi.string().required().min(1),
+        country: Joi.string().required(),
+        image: Joi.object({
+            url: Joi.string().uri().required()
+        }).required()
+
+    }).required()
 });
