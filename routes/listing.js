5d5d4cdc0614afe3b729376d6c6d85512a03f325
Judas-u <adishwarraj9905@gmail.com>
2025-07-18 13:46:50 +0530

    MVC - Model, View, Controller

--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,10 @@ const wrapAsync = require("../utils/wrapAsync");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
+const listingController = require("../controllers/listing.js");
+
 // Index route
-router.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index", { allListings });
-  })
-);
+router.get("/", wrapAsync(listingController.index));
 
 // New route
 router.get("/new", isLoggedIn, (req, res) => {
