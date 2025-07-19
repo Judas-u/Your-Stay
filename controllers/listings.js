c8cdb174805d9756159543de68bfca32aadbb655
Judas-u <adishwarraj9905@gmail.com>
2025-07-19 14:17:01 +0530

    Save Link in Mongo

--- controllers/listings.js
+++ controllers/listings.js
@@ -29,9 +29,14 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+	let url = req.file.path;
+	let filename = req.file.path;
+
 	const newListing = new Listing(req.body.listing);
 	newListing.owner = req.user._id;
+	newListing.image = {url , filename};
 	await newListing.save();
+
 	req.flash("success", "Successfully created a new listing!");
 	res.redirect("/listings");
 };
--- schema.js
+++ schema.js
@@ -2,22 +2,23 @@ const Joi = require('joi');
 
 // Listing Schema
 module.exports.listingSchema = Joi.object({
-  listing: Joi.object({
-    title: Joi.string().trim().required(),
-    description: Joi.string().trim().required(),
-    price: Joi.number().min(0).required(),
-    location: Joi.string().min(1).trim().required(),
-    country: Joi.string().trim().required(),
-    image: Joi.object({
-      url: Joi.string().uri().required()
-    }).required()
-  }).required()
+	listing: Joi.object({
+		title: Joi.string().trim().required(),
+		description: Joi.string().trim().required(),
+		price: Joi.number().min(0).required(),
+		location: Joi.string().min(1).trim().required(),
+		country: Joi.string().trim().required(),
+		image: Joi.object({
+			url: Joi.string().uri(),
+			filename: Joi.string()
+		}).optional()
+	}).required()
 });
 
 // Review Schema
 module.exports.reviewSchema = Joi.object({
-  review: Joi.object({
-    rating: Joi.number().min(1).max(5).required(),
-    comment: Joi.string().trim().required()
-  }).required()
+	review: Joi.object({
+		rating: Joi.number().min(1).max(5).required(),
+		comment: Joi.string().trim().required()
+	}).required()
 });
