0a098a3cac91969748cd15b66b4014ee1db3eff8
Judas-u <adishwarraj9905@gmail.com>
2025-07-27 15:54:51 +0530

    Map Popup

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,17 @@
-
 mapboxgl.accessToken = mapToken;
 
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-         style: "mapbox://styles/mapbox/streets-v12", // Style URL
-         center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    }
-);
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: listing.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
 
 const marker1 = new mapboxgl.Marker({ color: 'red', rotation: 0 })
-    .setLngLat(coordinates)
-    .addTo(map);
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `<h3>${listing.title}</h3><p>Exact Location provided after booking</p>`
+    )
+  )
+  .addTo(map);
