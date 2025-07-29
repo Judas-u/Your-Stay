68963305e2f9ae4b0c76c697c21eb2073c704bb6
Judas-u <adishwarraj9905@gmail.com>
2025-07-29 22:09:55 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-// console.log(process.env.SECRET) 
 
 const express = require("express");
 const app = express();
@@ -9,23 +8,20 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
-const ExpressError = require("./utils/ExpressError.js");
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-const dbUrl = process.env.ATLASDB_URL;
-
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/reviews.js");
-const userRouter = require("./routes/user.js");
-
 const session = require("express-session");
-const MongoStore = require('connect-mongo')
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
-
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
+
+const ExpressError = require("./utils/ExpressError");
+const User = require("./models/user");
+
+const listingRouter = require("./routes/listing");
+const reviewRouter = require("./routes/reviews");
+const userRouter = require("./routes/user");
+
+const dbUrl = process.env.ATLASDB_URL;
 
 // Connect to MongoDB
 async function main() {
@@ -39,25 +35,27 @@ main()
     console.error("MongoDB connection error:", err);
   });
 
-// Configure EJS
+// View Engine Setup
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-// cookies Session
+// Session Config
 const store = MongoStore.create({
- mongoUrl: dbUrl,
- crypto: {
-   secret : process.env.SECRET
- },
- touchAfter: 24 * 3600,
-})
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
 
-store.on("error" , (err) => {
-  console.log("Error in MONGO SESSION STORE", err)
+store.on("error", (err) => {
+  console.log("Error in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
@@ -72,51 +70,47 @@ const sessionOptions = {
   },
 };
 
-// Basic route
-// app.get("/", (req, res) => {
-//   res.send("Hi I am root!");
-// });
-
-
 app.use(session(sessionOptions));
 app.use(flash());
 
-// Passport
+// Passport Config
 app.use(passport.initialize());
 app.use(passport.session());
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Flash + User Info Middleware
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user; 
+  res.locals.currUser = req.user;
   next();
 });
 
-
-//  Path -> routes/listing
+// Routes
 app.use("/listings", listingRouter);
-//  Path -> routes/reviews
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
+
+// ✅ Root Route for Render and manual test
+app.get("/", (req, res) => {
+  res.send("🏡 WanderLust is Live!");
+});
 
 // 404 Handler
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page not found!", 404));
 });
 
-// Error Handler
+// Global Error Handler
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
-  console.error("Error:", err); // log full error
+  console.error("Error:", err);
   res.status(statusCode).render("error", { message: err.message || "Something went wrong" });
 });
 
-
-// Start server
+// Start Server
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
 });
